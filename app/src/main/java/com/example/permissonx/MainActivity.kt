f73335b9bddf4dd78dc67e5379116adb29f7833c
Yuhong <yuhongdeyx@163.com>
2022-06-13 15:24:32 +0800

    "完成PermissionX库的开发与测试工作"

--- app/src/main/java/com/example/permissonx/MainActivity.kt
+++ app/src/main/java/com/example/permissonx/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.permissonx
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.permissionx.yuhong.PermissionCallback
+import com.permissionx.yuhong.PermissionX
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val makeCallBtn = findViewById<Button>(R.id.makeCallBtn)
+        makeCallBtn.setOnClickListener{
+            PermissionX.request(this,
+            android.Manifest.permission.CALL_PHONE,callback = {
+                        allGranted:Boolean,deniedList:List<String> -> Unit
+                    if (allGranted){
+                        call()
+                    }else{
+                        Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+        }
+    }
+
+
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        }catch (e: SecurityException){
+            e.printStackTrace()
+        }
     }
 }
